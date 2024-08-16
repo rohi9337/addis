112bb04da73b88d8364fce323c4448d9f7be2e0c
rohi9337 <mengstuyaregal9337@gmail.com>
2024-08-16 22:38:02 +0300

    hgj

--- addis_city/settings.py
+++ addis_city/settings.py
@@ -30,7 +30,8 @@ DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['addis-city.onrender.com'] 
 
 
 # Application definition
